f2d4595e562e481167a4cd7177049886537d8fb5
Vladimir Boychentsov <sakrist@MacBook-Pro.local>
2011-05-20 11:15:54 +0300

    Initial Commit

---
+++ One minute/One_minuteAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  One_minuteAppDelegate.h
+//  One minute
+//
+//  Created by Vladimir Boychentsov on 5/20/11.
+//  Copyright 2011 www.injoit.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface One_minuteAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
---
+++ One minute/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  One minute
+//
+//  Created by Vladimir Boychentsov on 5/20/11.
+//  Copyright 2011 www.injoit.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[])
+{
+    NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+}
